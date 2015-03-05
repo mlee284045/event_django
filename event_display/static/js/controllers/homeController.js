2070d55a2488af2974a8a2d8d00406e96a772b03
Mike Lee <mike.seung.lee@gmail.com>
2015-03-04 17:31:40 -0800

    fixed remove

--- event_display/static/js/controllers/homeController.js
+++ event_display/static/js/controllers/homeController.js
@@ -1,11 +1,6 @@
 function homeController($scope, $http) {
-    console.log('home');
 
     var target;
-
-    $scope.testing = function() {
-        console.log('testing completed!!!!!')
-    };
 
     $scope.urlSearch = function() {
         target = $scope.targetUrl;
@@ -15,17 +10,16 @@ function homeController($scope, $http) {
 
         $http.post('/scrape/', target).
             success(function(data, status, headers, config) {
-                console.log('success');
                 console.log(data);
                 $scope.list = data;
             }).
             error(function(data, status, headers, config) {
                 console.log(data);
-                console.log('failed');
             });
     };
 
     $scope.removeItem = function(item) {
+        var eventList = $scope.list;
         var idx = eventList.indexOf(item);
         eventList.splice(idx, 1);
     };
